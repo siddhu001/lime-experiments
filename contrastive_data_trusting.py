c3c39f725a7dda76a003c5693d91fd71ea1c32f9
Vijay Viswanathan <vcomeinpeace@gmail.com>
2021-05-08 21:19:44 -0400

    Fix possible clash in regressor

--- contrastive_data_trusting.py
+++ contrastive_data_trusting.py
@@ -78,7 +78,7 @@ def main():
   LIME = LimeTextExplainer(class_names=class_names, mode="classification") 
 
   ridge_regressor = Ridge(alpha=1, fit_intercept=True, random_state=0)
-  model_regressor = ridge_regressor
+  model_regressor = Ridge(alpha=1, fit_intercept=True, random_state=0)
   regressor_requires_positive_values=False
 
   sigmas = {'multi_polarity_electronics': {'neighbors': 0.75, 'svm': 10.0, 'tree': 0.5,
